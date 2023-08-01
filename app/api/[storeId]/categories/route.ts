d1436123ccddb6f5531b41e3e0b7f13f59c8f055
GenDev0 <chebbi.ahmed.info@gmail.com>
2023-08-01 17:59:13 +0100

    finish categories and add sizes model and routes

--- app/api/[storeId]/categories/route.ts
+++ app/api/[storeId]/categories/route.ts
@@ -9,15 +9,15 @@ export async function POST(
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { label, imageUrl } = body;
+    const { name, billboardId } = body;
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
     }
-    if (!label) {
-      return new NextResponse("Label is required", { status: 400 });
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
     }
-    if (!imageUrl) {
-      return new NextResponse("Image URL is required", { status: 400 });
+    if (!billboardId) {
+      return new NextResponse("Billboard Id is required", { status: 400 });
     }
     if (!params.storeId) {
       return new NextResponse("Store Id is required", { status: 400 });
@@ -34,16 +34,16 @@ export async function POST(
       return new NextResponse("Unauthorized", { status: 400 });
     }
 
-    const billboard = await prismadb.billboard.create({
+    const category = await prismadb.category.create({
       data: {
-        label,
-        imageUrl,
+        name,
+        billboardId,
         storeId: params.storeId,
       },
     });
-    return NextResponse.json(billboard);
+    return NextResponse.json(category);
   } catch (error) {
-    console.log("[BILLBOARDS_POST]", error);
+    console.log("[CATEGORIES_POST]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
@@ -57,14 +57,14 @@ export async function GET(
       return new NextResponse("Store Id is required", { status: 400 });
     }
 
-    const billboards = await prismadb.billboard.findMany({
+    const categories = await prismadb.category.findMany({
       where: {
         storeId: params.storeId,
       },
     });
-    return NextResponse.json(billboards);
+    return NextResponse.json(categories);
   } catch (error) {
-    console.log("[BILLBOARDS_GET]", error);
+    console.log("[CATEGORIES_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
